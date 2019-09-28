8a72722352749c759514f1a2b38e50c1299701e0
Brian Otieno <bryomajor@gmail.com>
2019-09-29 02:21:25 +0300

    Add a function that searches for users' profiles.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,9 +9,23 @@ import { ProfileService } from '../profile-service/profile.service';
 export class ProfileComponent implements OnInit {
   profile: any[];
   repos: any[];
+  visitorUsername: string;
 
   constructor(private _profileService: ProfileService) {
     this._profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+
+    this._profileService.getRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
+   }
+
+   findProfile() {
+     this._profileService.updateProfile(this.visitorUsername);
+     this._profileService.getProfileInfo().subscribe(profile => {
       console.log(profile);
       this.profile = profile;
     });
