05a58f29c54d0708089beb0d47fd0aec1819b4e2
Brian Otieno <bryomajor@gmail.com>
2019-09-28 15:57:45 +0300

    Create a service which will store profile data to serve the app with.

---
+++ src/app/services/profile-service.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileServiceService {
+
+  constructor() { }
+}
