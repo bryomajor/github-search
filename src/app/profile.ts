0f571005a4e5749a486a2f30795dd6353aa8c9c9
Brian Otieno <bryomajor@gmail.com>
2019-09-30 10:30:20 +0300

    Add class for the profile.

---
+++ src/app/profile.ts
@@ -0,0 +1,4 @@
+export class Profile {
+
+    constructor(public name: string, public login: string, public bio: string, public hireable: string, public blog: string, public email: string, public followers: number, public following: number, public public_gists: number, public public_repos: number) {}
+}
